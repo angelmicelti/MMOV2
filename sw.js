2b7f5a31dd613bab1743edccf704c15335b6d8dd
angelmicelti <angelmicelti@users.noreply.github.com>
2026-07-07 19:47:51 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,46 @@
+const CACHE_NAME = 'qr-scanner-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/styles.css',
+    '/app.js',
+    '/firebase-config.js',
+    '/manifest.json',
+    'https://unpkg.com/html5-qrcode'
+];
+
+// Instalación
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(urlsToCache))
+    );
+});
+
+// Activación - limpiar caches antiguas
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(names => {
+            return Promise.all(
+                names.filter(name => name !== CACHE_NAME)
+                    .map(name => caches.delete(name))
+            );
+        })
+    );
+});
+
+// Estrategia: Network First (siempre intenta red, si falla usa cache)
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        fetch(event.request)
+            .then(response => {
+                // Clonar y guardar en cache
+                const responseClone = response.clone();
+                caches.open(CACHE_NAME).then(cache => {
+                    cache.put(event.request, responseClone);
+                });
+                return response;
+            })
+            .catch(() => caches.match(event.request))
+    );
+});
